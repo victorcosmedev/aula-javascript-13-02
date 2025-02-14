38d8a0279945cb9b2ca84fcabeeae335a3627d7f
victorcosmedev <victorcosme.profissional@gmail.com>
2025-02-13 22:14:59 -0300

    Exercício 2 - Funções e escopo

--- funcoes.js
+++ funcoes.js
@@ -10,5 +10,27 @@ const converterParaMaiusculas = (string) => {
     return string.toUpperCase();
 }
 
-console.log(mensagem); // Neste caso, 'mensagem' is not defined porque ela só existe dentro do escopo da função.
+// console.log(mensagem); // Neste caso, 'mensagem' is not defined porque ela só existe dentro do escopo da função.
 
+
+// EXERCICIO 2
+function processarDados(funcao){
+    funcao();
+}
+
+processarDados(() => {
+    console.log("Processo concluído.");
+});
+
+
+function buscarUsuario(){
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve({nome: "Victor", id: 1})
+        });
+    })
+}
+
+buscarUsuario().then((response) => {
+    console.log(response);
+})
