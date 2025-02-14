1b8db41be683fb7bb15532158242f3de736111ed
victorcosmedev <victorcosme.profissional@gmail.com>
2025-02-13 21:35:00 -0300

    Exercício de variáveis concluído.

---
+++ variaveis.js
@@ -0,0 +1,18 @@
+// EXERCÍCIO 1
+
+const nome = "Victor Nascimento Cosme";
+let idade = 19;
+const aprovado = true;
+const notas = [10, 8, 7];
+let usuario = {
+    email: "rm558856@fiap.com.br",
+    senha: "senha123",
+};
+
+console.log(nome);
+console.log(idade);
+console.log(notas);
+console.log(usuario);
+
+// A diferença entre null e undefined é que null é um valor definido de forma proposital
+// Já o undefined ocorre quando a variável não recebe valor algum.
